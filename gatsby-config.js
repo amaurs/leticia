6bdcdde629fef2c5794e85d7b42ea7dd899a5d8a
amaurs <amaury.gtz@gmail.com>
2020-08-16 13:57:27 -0700

    🐟 First entry, everything ready from template.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `recipees`,
+        path: `${__dirname}/src/recipees`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,8 +25,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `minimal-ui`
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
